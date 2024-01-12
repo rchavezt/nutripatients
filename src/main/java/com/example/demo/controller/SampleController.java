b6325e2749db218ec7ca41b3195e79e06b1f2e09
Vania Reyes <vania_k77@hotmail.com>
2024-01-12 12:46:42 -0600

    Change controller and service to add three new methods

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 import com.example.demo.dao.PatientDao;
 import com.example.demo.service.SampleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController("/paciente")
 public class SampleController {
@@ -14,5 +16,20 @@ public class SampleController {
     @PostMapping("/guardar")
     public void savePatient(@RequestBody PatientDao patientDao){
         sampleService.savePatient(patientDao);
+    }
+
+    @GetMapping("/buscar")
+    public String searchPatient(@RequestParam Integer id){
+        return sampleService.getPatient(id);
+    }
+
+    @DeleteMapping
+    public String deletePatient(@RequestParam Integer id){
+        return sampleService.deletePatient(id);
+    }
+
+    @GetMapping("/verTodos")
+    public ResponseEntity<List<PatientDao>> listAllPatient(){
+        return ResponseEntity.ok(sampleService.listAllPatient());
     }
 }
--- src/main/java/com/example/demo/service/SampleService.java
+++ src/main/java/com/example/demo/service/SampleService.java
@@ -3,14 +3,35 @@ package com.example.demo.service;
 import com.example.demo.dao.PatientDao;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class SampleService {
+    String status;
     Map<Integer, PatientDao> pacientes = new HashMap<>();
+    List<PatientDao> patientDaoList =new ArrayList<>();
     public void savePatient(PatientDao patient) {
         pacientes.put(pacientes.size() + 1, patient);
         System.out.println("Paciente registrado: " + patient.getNombre());
+    }
+
+    public String getPatient(Integer id){
+        return pacientes.get(id).getNombre();
+    }
+
+    public String deletePatient(Integer id){
+        if(pacientes.containsKey(id)){
+            status = "Paciente "+ pacientes.get(id).getNombre() + " eliminado";
+            pacientes.remove(id);
+            System.out.println(status);
+        }else{
+            status = "No existe el paciente";
+            System.out.println(status);
+        }
+        return status;
+    }
+
+    public List<PatientDao> listAllPatient(){
+        return new ArrayList<>(pacientes.values());
     }
 }
